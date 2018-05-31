1816c56463343abc34020273eee7f4919092eced
Michel van Gruijthuijsen <michel@mistige.com>
2018-05-31 15:57:15 +0200

    fix gumballmachine

--- GumballMachine.php
+++ GumballMachine.php
@@ -16,6 +16,6 @@ class GumballMachine{
 
     // The user turns the wheel, machine dispenses gumball!
     public function turnWheel(){
-        $this->setGumballs($this->getGumballs() - 16 );
+        $this->setGumballs($this->getGumballs() - 1 );
     }
 }
